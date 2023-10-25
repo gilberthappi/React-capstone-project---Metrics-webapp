09ccd2ba1d9e498508084145620fa17366f76c65
gilberthappi <gdushimimana6@gmail.com>
2023-10-25 10:14:52 +0200

    initialization of project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/extensions */
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Coins from './routes/Coins';
+import SpecificCoin from './routes/SpecificCoin';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Coins />} />
+          <Route path="coin/:id" element={<SpecificCoin />} />
+          {' '}
+
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
